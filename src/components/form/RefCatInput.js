e822f20f0696fba1e9d340f04eb7837d8c2b1ffc
Iván Peña Huguet <ivanpenyahuguet@gmail.com>
2021-06-30 00:03:21 +0200

    Get ref cat from codes

--- src/components/form/RefCatInput.js
+++ src/components/form/RefCatInput.js
@@ -2,19 +2,34 @@ import { useEffect } from 'react'
 import { useFormikContext } from 'formik'
 import TextField from './TextField'
 import RefCatToLocation from '../../services/helpers/RefCatToLocation'
+import LocationToRefCat from '../../services/helpers/LocationToRefCat'
 import { GetFieldsAreas } from '../../services/RequestService'
 
 export default function RefCatInput({ name, label }) {
   const { values, setValues, errors } = useFormikContext()
-  const { refCast } = values
+  const { refCast, province, town, polygon, parcel } = values
   const refCastError = errors.refCast
 
   useEffect(async () => {
-    if (refCast && refCast.length == 20 && !refCastError) {
+    if (refCast && refCast.length == 20 && !refCastError && !parcel) {
       const areas = await GetFieldsAreas(refCast)
       setValues({ ...values, ...RefCatToLocation(refCast), ...areas }, false)
     }
   }, [refCast, refCastError, setValues])
+
+  useEffect(() => {
+    console.log('a')
+    if (province && town && polygon && parcel) {
+      console.log('b')
+      setValues(
+        {
+          ...values,
+          refCast: LocationToRefCat({ province, town, polygon, parcel }),
+        },
+        false
+      )
+    }
+  }, [province, town, polygon, parcel, setValues])
 
   return <TextField name={name} label={label} />
 }
--- src/services/helpers/LocationToRefCat.js
+++ src/services/helpers/LocationToRefCat.js
@@ -1,15 +1,15 @@
 import { calculoLetrasReferenciaCatastral } from '../validators/ValidateRefCat'
 
-export default function RefCatToLocation(inputObject) {
+export default function LocationToRefCat(inputObject) {
   let refCat = ''
-
-  refcat
-  const objToReturn = {
-    province: parseInt(refCat.substr(0, 2)),
-    town: parseInt(refCat.substr(2, 3)),
-    polygon: parseInt(refCat.substr(6, 3)),
-    parcel: parseInt(refCat.substr(9, 5)),
-  }
-
+  console.log(inputObject)
+  const { province, town, polygon, parcel } = inputObject
+  refCat += province.toString().padStart(2, '0')
+  refCat += town.toString().padStart(3, '0')
+  refCat += 'A'
+  refCat += polygon.toString().padStart(3, '0')
+  refCat += parcel.toString().padStart(5, '0')
+  refCat += '0000'
+  refCat += calculoLetrasReferenciaCatastral(refCat)
   return refCat
 }
